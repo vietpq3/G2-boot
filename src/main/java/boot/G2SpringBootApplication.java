5dd8db2c2c9e7c0733a3d5c220b77c8df5f3b418
G2-boot <quocviet.nonwork@gmail.com>
2018-04-06 01:33:14 +0700

    update

--- src/main/java/boot/G2SpringBootApplication.java
+++ src/main/java/boot/G2SpringBootApplication.java
@@ -1,13 +1,19 @@
 package boot;
 
+import java.util.Locale;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 @SpringBootApplication(scanBasePackages = { "controller", "logicImpl", "daoImpl" })
 public class G2SpringBootApplication {
@@ -28,8 +34,25 @@ public class G2SpringBootApplication {
 		dataSource.setUsername(env.getProperty("spring.datasource.username"));
 		dataSource.setPassword(env.getProperty("spring.datasource.password"));
 
-		System.out.println("## getDataSource: " + dataSource);
-
 		return dataSource;
+	}
+
+	@Bean(name = "messageSource")
+	public MessageSource initMessageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+		messageSource.setBasename(env.getProperty("spring.messages.basename"));
+		messageSource.setCacheSeconds(Integer.parseInt(env.getProperty("spring.messages.cache-duration")));
+		
+		return messageSource;
+	}
+
+	@Bean(name = "localeResolver")
+	public LocaleResolver initLocaleResolver() {
+		CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+//		localeResolver.setDefaultLocale(new Locale(env.getProperty("spring.mvc.locale")));
+		localeResolver.setDefaultLocale(Locale.ENGLISH);
+
+		return localeResolver;
 	}
 }
